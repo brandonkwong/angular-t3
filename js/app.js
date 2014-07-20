c370e941aacfe941cff8343c18d4b2afccb356b0
brandonkwong <brandonjk@gmail.com>
2014-07-19 17:07:47 -0700

    Changed classes and click function to be more responsive to Angular

--- js/app.js
+++ js/app.js
@@ -8,104 +8,53 @@ t3App.controller('T3Controller', function($scope) {
   $scope.playerTwo = 0;
 
   // Difficulty Level
-  $scope.levelEasy = 3;
-  $scope.levelNormal = 4;
-  $scope.levelHard = 5;
+  $scope.levelNorm = 3;
+  $scope.levelHard = 4;
+  $scope.levelUlti = 5;
   
   // Board Init
   $scope.boardInit = function(grid) {
     // Board Array
     $scope.board = [];
-
-    // X Board Test
-    $scope.playerX = [];
-
-    for (var r = 0; r < grid; r++) {
-      // Row Arrays
+    // Row For-loop
+    for (var i = 0; i < grid; i++) {
+      // Row Array
       var row = [];
-
-      // X Rows Test
-      var rowX = [];
-      
-      for (var c = 0; c < grid; c++) {
-        // Tile Objects
+      // Tile For-loop
+      for (var j = 0; j < grid; j++) {
+        // Push Tiles into Row
         row.push(
           {
-            key: '['+r+']'+'['+c+']',
+            key: '['+i+']'+'['+j+']',
             active: false,
-            iconTimes: false,
-            iconCircle: false,
-            // iconHeart: false,
-            icon: null,
-            score: 0
+            playerOne: false,
+            playerTwo: false
           }
         );
-
-        // X Tiles Test
-        rowX.push(
-          {
-            icon: 'times'
-          }
-        );
-
       }
-
+      // Push Rows into Board
       $scope.board.push(row);
-
-      // X Rows Push Test
-      $scope.playerX.push(rowX);
-
     }
-  }
-  // To be implemented via ng-model or option
-  $scope.boardInit($scope.levelEasy);
+  }($scope.levelNorm);
 
 
-  // Function for Tiles
-  $scope.tileIcon = function(tile) {
-
+  // Tile function
+  $scope.keyClick = function(tile) {
     if (tile.active == false) {
       tile.active = true;
-
       switch ($scope.playerActive) {
         // Player 1 Actions on Click
         case 1:
-          tile.icon = 'times';
-          tile.iconTimes = true;
+          tile.playerOne = true;
           $scope.playerActive = 2;
-
-          // Player X Win Testing
-          // $scope.playerOne.push(0);
-
-          // if ($scope.board[0][0].score == $scope.board.length) {
-          //   alert('yes');
-          // }
-          // else {
-          //   alert('nope');
-          // }
-
-          // console.log($scope.board[0][0].icon);
-          // console.log($scope.playerX[0][0].icon);
-
-          // for (var i = 0; i < $scope.board.length; i++)
-          //   if ($scope.board[0][i].item == $scope.playerX[0][i].item) {
-          //     alert('success');
-          //   }
-          //   else {
-          //     alert('fail');
-          //   }
-          // }
-
           break;
         // Player 2 Actions on Click
         case 2:
-          tile.icon = 'circle';
-          tile.iconCircle = true;
+          tile.playerTwo = true;
           $scope.playerActive = 1;
           break;
         // case 3:
-        //   tile.icon = 'heart';
-        //   tile.iconHeart = true;
+        //   tile.playerThree = true;
         //   $scope.playerActive = 1;
         //   break;
       }
@@ -113,7 +62,6 @@ t3App.controller('T3Controller', function($scope) {
     else {
       alert('Don\'t do it, Bro');
     }
-
   };
 
 });
