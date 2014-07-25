62d61c8278b6caef1889ce1df1a4c86160248368
brandonkwong <brandonjk@gmail.com>
2014-07-25 16:39:16 -0700

    Implemented Firebase connection and made minor function improvements

--- js/app.js
+++ js/app.js
@@ -2,9 +2,32 @@ var t3App = angular.module('T3App', ['firebase']);
 
 t3App.controller('T3Controller', ['$scope', '$firebase', function($scope, $firebase) {
 
-  // Firebase Connection
-  var t3Fire = new Firebase('https://angular-t3-test.firebaseio.com');
-  
+  // Firebase Reference URL
+  var ref = 'https://test-ttt-08.firebaseio.com/';
+
+  // Firebase Remote References
+  $scope.remoteBoard = $firebase(new Firebase(ref + 'board'));
+  $scope.remoteBoardActive = $firebase(new Firebase(ref + 'boardActive'));
+  $scope.remoteBoardScore = $firebase(new Firebase(ref + 'boardScore'));
+  $scope.remoteGameInit = $firebase(new Firebase(ref + 'gameInit'));
+  $scope.remoteLevel = $firebase(new Firebase(ref + 'level'));
+  $scope.remotePlayer = $firebase(new Firebase(ref + 'player'));
+  $scope.remoteStatus = $firebase(new Firebase(ref + 'status'));
+  $scope.remoteWinScore = $firebase(new Firebase(ref + 'winScore'));
+
+  // Note: change urls to snake_case
+ 
+  // Firebase Bindings
+  $scope.remoteBoard.$bind($scope, 'board');
+  $scope.remoteBoardActive.$bind($scope, 'boardActive');
+  $scope.remoteBoardScore.$bind($scope, 'boardScore');
+  $scope.remoteGameInit.$bind($scope, 'gameInit');
+  $scope.remoteLevel.$bind($scope, 'level');
+  $scope.remotePlayer.$bind($scope, 'playerActive');
+  $scope.remoteStatus.$bind($scope, 'boardStatus');
+  $scope.remoteWinScore.$bind($scope, 'winScore');
+
+
   // Difficulty Level
   $scope.lvlNorm = 3;
   $scope.lvlHard = 4;
@@ -18,11 +41,11 @@ t3App.controller('T3Controller', ['$scope', '$firebase', function($scope, $fireb
     $scope.gameInit = true;
     $scope.boardActive = true;
     $scope.boardStatus = 'Player Move';
-    $scope.playerActive = 1;
+    $scope.playerActive = Math.floor((Math.random() * 2) + 1);
     $scope.level = lvl;
 
     // Board Score
-    $scope.boardScore = [];
+    $scope.boardScore = {tileCount: 0};
     $scope.winScore;
 
     // Win Score Set by Level
@@ -43,9 +66,9 @@ t3App.controller('T3Controller', ['$scope', '$firebase', function($scope, $fireb
       // Clear Tile
       if (i % lvl === 0) {
         $scope.board.push({
-          index: '['+i+']', // Index for Testing
+          index: i,
           active: false,
-          wallL: true, // Clears Floats
+          wallL: true, // Clears floats to break into rows
           wallR: false,
           playerOne: false,
           playerTwo: false
@@ -53,7 +76,7 @@ t3App.controller('T3Controller', ['$scope', '$firebase', function($scope, $fireb
       }
       else if (i % lvl === lvl - 1) {
         $scope.board.push({
-          index: '['+i+']',
+          index: i,
           active: false,
           wallL: false,
           wallR: true,
@@ -64,7 +87,7 @@ t3App.controller('T3Controller', ['$scope', '$firebase', function($scope, $fireb
       // Default Tile
       else {
         $scope.board.push({
-          index: '['+i+']',
+          index: i,
           active: false,
           wallL: false,
           wallR: false,
@@ -90,11 +113,9 @@ t3App.controller('T3Controller', ['$scope', '$firebase', function($scope, $fireb
   // Tile Scan Function for Win Conditions
   $scope.tileScan = function(tile, player) {
 
-    // Get Index of Clicked Tile
-    var index = $scope.board.indexOf(tile);
-
     // Scan Variables
     var b = $scope.board;
+    var index = tile.index;
     var grid = $scope.level * $scope.level;
 
     // Player Arrays
@@ -205,7 +226,7 @@ t3App.controller('T3Controller', ['$scope', '$firebase', function($scope, $fireb
       }
     }
 
-    // Win
+    // Win Conditions
     if ($scope.playerRow.length >= $scope.winScore) {
       $scope.boardStatus = 'Player Wins';
       $scope.boardActive = false;
@@ -222,8 +243,8 @@ t3App.controller('T3Controller', ['$scope', '$firebase', function($scope, $fireb
       $scope.boardStatus = 'Player Wins';
       $scope.boardActive = false;
     }
-    // Tie
-    else if ($scope.boardActive && $scope.boardScore.length === grid) {
+    // Tie Condition
+    else if ($scope.boardActive && $scope.boardScore.tileCount === grid) {
       $scope.boardStatus = 'Both Players Tie';
       $scope.boardActive = false;
       $scope.gameInit = false;
@@ -235,8 +256,8 @@ t3App.controller('T3Controller', ['$scope', '$firebase', function($scope, $fireb
   // Tile Click Function
   $scope.tileClick = function(tile, player) {
 
-    if (tile.active === false) {
-      $scope.boardScore.push(0);
+    if (!tile.active) {
+      $scope.boardScore.tileCount++;
       tile.active = true;
       switch ($scope.playerActive) {
         // Player 1 Actions on Click
@@ -244,8 +265,8 @@ t3App.controller('T3Controller', ['$scope', '$firebase', function($scope, $fireb
           tile.playerOne = true;
           $scope.tileScan(tile, 'playerOne');
           if ($scope.boardActive) {
-            $scope.boardStatus = 'Player Move';
             $scope.playerActive = 2;
+            $scope.boardStatus = 'Player Move';
           }
           break;
         // Player 2 Actions on Click
